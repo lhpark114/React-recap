cafe8cc9098913b533ccf4d9b2ab35d8dd5345e7
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-10 20:02:08 +0000

    jsx grammar (basic)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
 
 export default function App() {
+  const name = 'chocolate';
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
+    <>
+    <h1 className='pink'>Hello!</h1>
+      <h2>Bye!00</h2>
+      <p>{name}</p>
+      <ul>
+        <li>Chocolate</li>
+        <li>Strawberry</li>
+        <li>Banana</li>
+      </ul>
+      <img 
+        style={{width: '200px', height: '200px'}}
+        src='https://www.agiftinside.com/images/view/565/565/false/img/single_products/AGI-ACD2037-main-2018-1000.jpg'
+        alt='chocolatestrawberry'
+        />
+    </>
   )
 }
