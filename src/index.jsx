511cd67f5d30c0bc2058a81b899c0e6680a037e9
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-12 20:36:36 +0000

    Pointer_init

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppProducts from './AppProducts'
+import AppXY from './AppXY'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<AppProducts />
+		<AppXY />
 	</React.StrictMode>
 )
