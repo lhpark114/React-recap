e60f6667fa0408cff52472895aa40ede05492a90
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-13 02:50:01 +0000

    mouseCursor_part2 - x+y=position

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,17 +2,16 @@ import React, {useState} from 'react';
 import './AppXY.css'
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({x:0, y:0});
 
   return (
     <div className='container'
       onPointerMove={(e)=>{
         console.log(e.clientX, e.clientY);
-        setX(e.clientX);
-        setY(e.clientY);
+        setPosition({x:e.clientX, y:e.clientY});
+        // 만약 수평으로만 이동이 가능하도록 하려면?
       }}>
-      <div className='pointer' style={{transform: `translate(${x}px, ${y}px)`}}/>
+      <div className='pointer' style={{transform: `translate(${position.x}px, ${position.y}px)`}}/>
     </div>
   );
 }
