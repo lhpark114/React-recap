26d8ccbd798c7aa737ca1dd20ab400c6b85b91b4
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-13 14:18:51 +0000

    array status init

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppMentor from './AppMentor'
+import AppMentors from './AppMentors'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<AppMentor />
+		<AppMentors />
 	</React.StrictMode>
 )
