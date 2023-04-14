00d6a58eb29baef8018befd45fe60bb8fe02acc3
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-14 15:27:24 +0000

    reducer

---
+++ src/reducer/person-reducer.js
@@ -0,0 +1,30 @@
+export default function personReducer(person, action) {
+  switch(action.type) {
+    case'updated: {
+      const {prev, current} = action;
+      return {
+        ...person,
+          mentors: person.mentors.map((mentor) =>{
+            if(mentor.name === prev)
+            {return {...mentor, name: current};} return mentor;}),
+      };
+    }
+  case 'added':{
+    const {name, title} = action;
+    return {
+          ...person,
+          mentors: [{ name, title}, ...person.mentors],
+        };
+  }
+  case 'deleted': {
+    return : {
+          ...person,
+          mentors: person.mentors.filter((mentor) => 
+            mentor.name !== name),
+        };
+  }
+  default: {
+    throw Error(`알수없는 액션 타입 : ${action.type}`);
+  }
+  }
+}
