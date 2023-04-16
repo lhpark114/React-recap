6bea42857265c0c47fabab422a0d220428b84ec7
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-16 21:33:05 +0000

    useMemo

---
+++ src/AppMentorsButton.jsx
@@ -0,0 +1,82 @@
+import React, {useReducer, useMemo} from 'react';
+import personReducer from './reducer/person-reducer';
+import './AppXY.css'
+
+export default function AppMentors() {
+  //const [person, setPerson] = useState(initialPerson);
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
+  const handleUpdate = () => {
+        const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+        const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+        dispatch({type: 'updated', prev, current});
+        }
+  const handleAdd = () => {
+        const name = prompt(`추가할 사람의 이름은?`);
+        const title = prompt(`추가할 사람의 직함은?`);
+        dispatch({type: 'added', name, title});
+        }
+  const handleDelete = () => {
+        const name = prompt(`누구를 삭제하고 싶은가요?`);
+        dispatch({type: 'deleted', name});
+        }
+  return (
+    <div>
+      <h1>
+        {person.name}는 {person.title}
+      </h1>
+      <p>
+        {person.name}의 멘토는 :
+      </p>
+      <ul>
+        {person.mentors.map((mentor, index) =>(
+        <li key={index}>
+          {mentor.name} ({mentor.title})
+        </li>
+        ))}
+      </ul>
+      <Button text='멘토 이름 바꾸기'onClick={handleUpdate} />
+      <Button text='멘토 추가하기' onClick={handleAdd} />
+      <Button text='멘토 삭제하기' onClick={handleDelete} />
+    </div>
+  );
+}
+
+function Button({ text, onClick}) {
+  console.log('Button', text, 're-rendering');
+  const result = useMemo(() => calculateSomething(),[text])
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        backgroundColor:'black',
+        color: 'white',
+        borderRadius: '20px',
+        margin: '0.4rem',
+      }}
+      >
+      {`${text} ${result}`}
+    </button>
+  );
+}
+
+function calculateSomething() {
+  for (let i = 0; i < 10000; i++) {
+    console.log('😆');
+  }
+  return 10;
+}
+
+const initialPerson = {
+    name: '엘리',
+    title: '개발자',
+    mentors: [
+      {
+      name: '밥',
+      title: '시니어개발자',
+    },
+    {
+      name: '제임스',
+      title: '시니어개발자',
+    },
+    ],
+  }
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppTheme from './AppTheme'
+import AppMentorsButton from './AppMentorsButton'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<AppTheme />
+		<AppMentorsButton />
 	</React.StrictMode>
 )
