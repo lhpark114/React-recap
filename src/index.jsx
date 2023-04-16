e850dc4f47f054fe24ee9b8029ae733c4f94c433
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-16 19:16:13 +0000

    DarkMode init

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppCard from './AppCard'
+import AppTheme from './AppTheme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<AppCard />
+		<AppTheme />
 	</React.StrictMode>
 )
