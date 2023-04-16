687b1b0ee239e8ca9ceda824884d0c6da8f061ab
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-16 18:02:48 +0000

    Form _ useState (1)

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function AppForm() {
-  const handleSubmit=(e) =>{
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const handleSubmit=(e) => {
     e.preventDefault();
   };
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor='name'>name:</label>
-      <input type='text' id='name' name='name'/>
+      <input 
+        type='text' 
+        id='name' 
+        name='name' 
+        value={name} 
+        onChange={(e) => {
+        setName(e.target.value);
+      }}/>
       <label htmlFor='email'>email:</label>
-      <input type='email' id='email' name='email'/>
+      <input 
+        type='email' 
+        id='email' 
+        name='email'
+        value={email} 
+        onChange={(e) => {
+        setEmail(e.target.value);
+      }}/>
       <button>Submit</button>
     </form>
   );
