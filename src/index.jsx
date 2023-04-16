284d6213eff03a4064600f4fae98541ab684f43f
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-16 21:47:46 +0000

    Error, Loading

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppMentorsButton from './AppMentorsButton'
+import AppProducts from './basic/AppProducts'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<AppMentorsButton />
+		<AppProducts />
 	</React.StrictMode>
 )
